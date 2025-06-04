7ac551f60b3257c9941b4c610166e8b2a327d1ec
Devin Gupta <tsunamidev1@gmail.com>
2025-06-03 22:36:57 -0700

    more work

--- src/devin_experiments/franka/franka_env.py
+++ src/devin_experiments/franka/franka_env.py
@@ -1,16 +1,34 @@
 import gymnasium as gym
 import gymnasium_robotics
 from utils.custom_franka_env import CustomFrankaEnv
+from utils.simple_net import SimpleNet
+import torch
+from PIL import Image
 
 gym.register_envs(gymnasium_robotics)
 
-env = CustomFrankaEnv(gym.make('FrankaKitchen-v1', tasks_to_complete=['kettle'], render_mode='human'))
+env = CustomFrankaEnv(gym.make(
+    'FrankaKitchen-v1', 
+    object_noise_ratio=0.0, 
+    robot_noise_ratio=0.0, 
+    tasks_to_complete=['kettle'], 
+    render_mode='rgb_array'
+))
+
+print('observation space: ', env.observation_space)
+print('action space: ', env.action_space)
+
+actor = SimpleNet()
 
 obs, info = env.reset()
 for _ in range(50):
-    action = env.action_space.sample()  # Sample a random action
-    obs, reward, terminated, truncated, info = env.step(action)  # Take the action in the environment
-    print('reward: ', reward)
+    # action = actor(torch.tensor(obs))[0]
+    action = env.action_space.sample()
+    # obs, reward, terminated, truncated, info = env.step(action.detach().numpy())  # Take the action in the environment
+    obs, reward, terminated, truncated, info = env.step(action)
     env.render()  # Render the environment
 
-env.close()  # Close the environment when done
+    if terminated:
+        break
+
+env.close()
--- src/devin_experiments/franka/utils/custom_franka_env.py
+++ src/devin_experiments/franka/utils/custom_franka_env.py
@@ -2,12 +2,11 @@ import gymnasium as gym
 import gymnasium_robotics
 import numpy as np
 from gymnasium import spaces
+import matplotlib.pyplot as plt
+from PIL import Image
+import torchvision
+import torch
 
-# Register the environment
-# gym.register_envs(gymnasium_robotics)
-
-
-# --- Custom Environment Wrapper ---
 class CustomFrankaEnv(gym.Wrapper):
     """
     A custom wrapper for the FrankaKitchen-v1 environment to:
@@ -16,41 +15,58 @@ class CustomFrankaEnv(gym.Wrapper):
     """
     def __init__(self, env):
         super().__init__(env)
-        self.base_env = env # Store a reference to the base environment
-
-        # Define the new observation space based on 'observation' key
-        # Assuming 'observation' key holds a Box space.
-        # Access the observation space of the *unwrapped* environment to get the original structure
-        original_obs_space = self.base_env.observation_space['observation']
+        self.base_env = env
         self.observation_space = spaces.Box(
-            low=original_obs_space.low,
-            high=original_obs_space.high,
-            shape=original_obs_space.shape,
-            dtype=original_obs_space.dtype
+            low = -1.0, high = 1.0, shape = (480, 480), dtype = np.float32
         )
-        print(f"CustomFrankaEnv: New observation space shape: {self.observation_space.shape}")
+        self.action_space = self.base_env.action_space
 
+    def get_curr_image(self):
+        img = Image.fromarray(self.base_env.render())
+        img = torchvision.transforms.functional.rgb_to_grayscale(img, num_output_channels=1)
+        img = torchvision.transforms.functional.resize(img, (120, 120))
+        return np.array(img, dtype = np.float32)
+    
+    def get_goal_image(self):
+        img = Image.open('goal_image.png')
+        img = torchvision.transforms.functional.rgb_to_grayscale(img, num_output_channels=1)
+        img = torchvision.transforms.functional.resize(img, (120, 120))
+        return np.array(img, dtype = np.float32)
+    
+    def generate_observation(self):
+        curr_img = self.get_curr_image()
+        goal_img = self.get_goal_image()
+        assert curr_img.shape == goal_img.shape, 'Current image and goal image must have the same shape, but got {} and {}'.format(curr_img.shape, goal_img.shape)
+        diff_img = np.abs(curr_img - goal_img)
+        return diff_img
 
     def step(self, action):
         observation_dict, original_reward, terminated, truncated, info = self.base_env.step(action)
-        observation = observation_dict['observation']
+
+        original_reward -= 0.1
 
         kettle_dist = np.linalg.norm(observation_dict['achieved_goal']['kettle'] - observation_dict['desired_goal']['kettle'])
-        original_reward -= kettle_dist
+        if abs(kettle_dist - self.default_kettle_dist) > 0.0001:
+            original_reward += 1
+            print('kettle moved')
 
-        if kettle_dist < 0.05:  # Assuming a threshold for success
-            original_reward += 100.0 # Big reward for success!
-            print("Kettle task completed! Giving large reward.")
-
-        return observation, original_reward, terminated, truncated, info
+        return self.generate_observation(), original_reward, terminated, truncated, info
 
     def reset(self, **kwargs):
         observation_dict, info = self.base_env.reset(**kwargs)
-        # Return only the 'observation' array
-        return observation_dict['observation'], info
+
+        # store default kettle attributes for later stepping
+        self.default_kettle_dist = np.linalg.norm(observation_dict['achieved_goal']['kettle'] - observation_dict['desired_goal']['kettle'])
+        self.default_kettle_state = observation_dict['achieved_goal']['kettle']
+
+        return self.generate_observation(), info
 
     def render(self):
+        render_img = self.generate_observation()
+        plt.imshow(render_img)
+        plt.pause(0.01)
         return self.base_env.render()
 
     def close(self):
+        plt.close()
         return self.base_env.close()
